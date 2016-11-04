55a55b381032b789f4c712734fbf1792df6434e9
illl48 <mruncanny@gmail.com>
2016-11-04 14:18:40 -0700

    first

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,57 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by eddie on 2016/11/2.
+ */
+
+public class WordAdapter extends ArrayAdapter<Word>{
+    private int mColorResourceId;
+
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId){
+        super(context, 0, words);
+        mColorResourceId = colorResourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //return super.getView(position, convertView, parent);
+        View listItemView = convertView;
+        if(listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        Word currentWord = getItem(position);
+
+        ImageView imageImageView = (ImageView) listItemView.findViewById(R.id.image);
+        if(currentWord.hasImage()){
+            imageImageView.setImageResource(currentWord.getImageResourceId());
+            imageImageView.setVisibility(View.VISIBLE);
+        }
+        else{
+            imageImageView.setVisibility(View.GONE);
+        }
+
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        textContainer.setBackgroundColor(color);
+
+        return listItemView;
+    }
+}
